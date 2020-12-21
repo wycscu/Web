be6675dc3f21c57012dc59438c5ccd2f9f9e2a8b
wycscu <1245665627@qq.com>
2020-12-21 20:47:23 +0800

    202012221

--- js/animation.js
+++ js/animation.js
@@ -9,13 +9,35 @@ function getStyle(el,property){
 
 
 
-function animate(el,property,target){
-	setInterval(function(){
-		var current = parseInt(getStyle(el,property));
-		var speed = (target - current)/30;  //处理取整问题
-		speed = speed>0?Math.ceil(speed):Math.floor(speed);
-		el.style[property] = current + speed + 'px';
-		el.innerHTML = el.style[property];
+function animate(el,properties){
+	clearInterval(el.timerId);
+	//进入前先清楚
+	el.timerId = setInterval(function(){
+		
+		for (var property in properties){
+			var current;
+			var target = properties[property];
+			//var current = parseInt(getStyle(el,property));
+			if(property ==='opacity') {
+				current =Math.round(parseFloat(getStyle(el,'opacity'))*100) ;
+			}else{
+				current = parseInt(getStyle(el,property));
+				
+			}
+			var speed = (target - current)/30;  //处理取整问题
+			speed = speed>0?Math.ceil(speed):Math.floor(speed);
+			//为了防止speed叠加，需要清除之前的setInterval函数
+			if(property==='opacity'){
+				el.style.opacity = (current + speed)/100;
+			}else{
+					el.style[property] = current + speed + 'px';
+			}
+			
+		//	el.innerHTML = property + ':' + el.style[property];
+		}
+	
+		
+	
 	},20)  //阻尼
 }
 
